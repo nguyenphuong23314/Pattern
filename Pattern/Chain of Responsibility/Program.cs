b31027f7c550061b14e27a9d0003788fcb000b87
Pong <nguyenphuong23314@gmail.com>
2022-04-15 00:05:13 +0700

    add chain of responsibility pattern

---
+++ Pattern/Chain of Responsibility/Program.cs
@@ -0,0 +1,17 @@
+﻿using Chain_of_Responsibility;
+
+var monkey = new MonkeyHandler();
+var squirrel = new SquirrelHandler();
+var dog = new DogHandler();
+
+//monkey.SetNext(squirrel).SetNext(dog);
+squirrel.SetNext(monkey).SetNext(dog);
+
+// The client should be able to send a request to any handler, not
+// just the first one in the chain.
+Console.WriteLine("Chain: Monkey > Squirrel > Dog\n");
+Client.ClientCode(monkey);
+Console.WriteLine();
+
+Console.WriteLine("Subchain: Squirrel > Dog\n");
+Client.ClientCode(dog);
