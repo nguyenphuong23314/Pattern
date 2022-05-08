2eaa100b8974531c73ffdd929be86e1f89251f18
Pong <nguyenphuong23314@gmail.com>
2022-05-08 23:37:47 +0700

    Command pattern

---
+++ Pattern/Command/Program.cs
@@ -0,0 +1,9 @@
+﻿// The client code can parameterize an invoker with any commands.
+using Command;
+
+Invoker invoker = new Invoker();
+invoker.SetOnStart(new SimpleCommand("Say Hi!"));
+Receiver receiver = new Receiver();
+invoker.SetOnFinish(new ComplexCommand(receiver, "Send email", "Save report"));
+
+invoker.DoSomethingImportant();
