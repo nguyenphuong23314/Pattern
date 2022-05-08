e4f2aea35d97bedf4698dba3aba651259cfded37
Pong <nguyenphuong23314@gmail.com>
2022-05-08 22:58:20 +0700

    Strategy pattern

---
+++ Pattern/Strategy/ConcreteStrategyA.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    public class ConcreteStrategyA : IStrategy
+    {
+        public object DoAlgorithm(object data)
+        {
+            var list = data as List<string>;
+            list.Sort();
+
+            return list;
+        }
+    }
+}
